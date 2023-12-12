7b0d7a33beec6a13f9e93532127b86b67bc2c247
aizatdildabek <83627633+aizatdildabek@users.noreply.github.com>
2023-12-12 23:33:48 +0600

    adding all files

---
+++ quiz_data.py
@@ -0,0 +1,27 @@
+questions = [
+    {
+        "question": "Сколько планет в Солнечной системе?",
+        "options": ["7", "8", "9", "10"],
+        "correct_option": 1
+    },
+    {
+        "question": "Что такое молекула воды?",
+        "options": ["H2O", "CO2", "O2", "N2"],
+        "correct_option": 0
+    },
+    {
+        "question": "Какое самое большое озеро в мире?",
+        "options": ["Каспийское", "Виктория", "Байкал", "Гурон"],
+        "correct_option": 1
+    },
+    {
+        "question": "Сколько ног у паука?",
+        "options": ["6", "8", "10", "12"],
+        "correct_option": 1
+    },
+    {
+        "question": "Какое химическое обозначение у золота?",
+        "options": ["Au", "Ag", "Fe", "Cu"],
+        "correct_option": 0
+    }
+]
